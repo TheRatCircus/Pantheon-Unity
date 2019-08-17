3a71ce44c707242ddc713b5fc80a6551ba5a4097
Jerome Martina <martinaj@shernet.sheridancollege.ca>
2019-08-17 12:19:54 -0400

    Basic main menu added

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿// Main menu handler
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Dungeon 1");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
