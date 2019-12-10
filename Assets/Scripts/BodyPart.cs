a2cd4508608439a21c54038b09c946acd23ea1e3
Jerome Martina <martinaj@shernet.sheridancollege.ca>
2019-12-09 20:24:16 -0500

    SpeciesDefinition and BodyPart classes

---
+++ Assets/Scripts/BodyPart.cs
@@ -0,0 +1,44 @@
+﻿// BodyPart.cs
+// Jerome Martina
+
+using UnityEngine;
+using Pantheon.Components;
+
+namespace Pantheon
+{
+    public enum BodyPartType
+    {
+        Head,
+        Arms,
+        Legs,
+        Teeth
+    }
+
+    [System.Serializable]
+    public sealed class BodyPart
+    {
+        [SerializeField] private string id = "DEFAULT_BODYPART_ID";
+        [SerializeField] private string name = "DEFAULT_BODYPART_NAME";
+        [SerializeField] private BodyPartType type;
+        [SerializeField] private int moveSpeedModifier;
+        [SerializeField] private Melee melee;
+
+        public string ID => id;
+        public string Name => name;
+        public BodyPartType Type => type;
+        public int MoveSpeedModifier => moveSpeedModifier;
+        public Melee Melee => melee;
+
+        public BodyPart() { }
+
+        public BodyPart(string id, string name, BodyPartType type,
+            int moveSpeedModifier, Melee melee)
+        {
+            this.id = id;
+            this.name = name;
+            this.type = type;
+            this.moveSpeedModifier = moveSpeedModifier;
+            this.melee = melee;
+        }
+    }
+}
---
+++ Assets/Scripts/SpeciesDefinition.cs
@@ -0,0 +1,25 @@
+﻿// SpeciesDefinition.cs
+// Jerome Martina
+
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace Pantheon
+{
+    public sealed class SpeciesDefinition
+    {
+        public string ID { get; private set; }
+        public string Name { get; private set; }
+        public Sprite Sprite { get; private set; }
+        public BodyPart[] Parts { get; private set; }
+
+        [JsonConstructor]
+        public SpeciesDefinition(string id, string name, Sprite sprite, BodyPart[] parts)
+        {
+            ID = id;
+            Name = name;
+            Sprite = sprite;
+            Parts = parts;
+        }
+    }
+}
