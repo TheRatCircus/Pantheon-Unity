a8e2d558bf965af8eb177d33e581fa4f4ed1203d
Jerome Martina <martinaj@shernet.sheridancollege.ca>
2019-12-09 20:23:46 -0500

    Species and Melee components

---
+++ Assets/Scripts/Components/Species.cs
@@ -0,0 +1,21 @@
+﻿// Species.cs
+// Jerome Martina
+
+namespace Pantheon.Components
+{
+    [System.Serializable]
+    public sealed class Species : EntityComponent
+    {
+        public SpeciesDefinition SpeciesDef { get; set; }
+
+        public Species(SpeciesDefinition def)
+        {
+            SpeciesDef = def;
+        }
+
+        public override EntityComponent Clone()
+        {
+            return new Species(SpeciesDef);
+        }
+    }
+}
