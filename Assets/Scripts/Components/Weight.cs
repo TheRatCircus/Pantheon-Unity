a5bb380431b6765d228d21a7e925519c06ecd619
Jerome Martina <martinaj@shernet.sheridancollege.ca>
2019-12-14 16:13:18 -0500

    Basic Size and Weight components

---
+++ Assets/Scripts/Components/Weight.cs
@@ -0,0 +1,24 @@
+﻿// Weight.cs
+// Jerome Martina
+
+using UnityEngine;
+
+namespace Pantheon.Components
+{
+    /// <summary>
+    /// Entity's weight in kilograms.
+    /// </summary>
+    public sealed class Weight : EntityComponent
+    {
+        [SerializeField] private int value;
+        public int Value
+        {
+            get => value;
+            set => this.value = Mathf.Clamp(value, 0, int.MaxValue);
+        }
+
+        public Weight(int value) => this.value = value;
+
+        public override EntityComponent Clone() => new Weight(value);
+    }
+}
