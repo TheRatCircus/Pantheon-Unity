96bfd766f6f137fb0487de9c660582d13d29c141
Jerome Martina <martinaj@shernet.sheridancollege.ca>
2019-12-14 00:07:13 -0500

    Some unit tests for cell walkable/blocking checks
    This is mostly so that the project is known to be set up for further unit testing, and to get into a test-driven mindset.
    
    Also, be aware that the project is now divided into assemblies.

---
+++ Assets/Tests/CellTests.cs
@@ -0,0 +1,45 @@
+﻿// CellTests.cs
+// Jerome Martina
+
+using NUnit.Framework;
+using Pantheon;
+using Pantheon.World;
+using UnityEngine;
+
+namespace PantheonTests
+{
+    public class CellTests
+    {
+        /// <summary>
+        /// Test that a default cell with a default terrain and an actor is
+        /// considered walkable.
+        /// </summary>
+        [Test]
+        public void DefaultCellWithActorIsWalkable()
+        {
+            Cell cell = new Cell(new Vector2Int(0, 0))
+            {
+                Actor = new Entity("TEST_ENTITY"),
+                Terrain = ScriptableObject.CreateInstance<TerrainDefinition>()
+            };
+            Assert.True(Cell.Walkable(cell));
+        }
+
+        /// <summary>
+        /// Test that a default cell with an actor is considered blocked.
+        /// </summary>
+        [Test]
+        public void CellWithActorIsBlocked()
+        {
+            Cell cell = new Cell(new Vector2Int(0, 0))
+            {
+                Actor = new Entity("TEST_ENTITY")
+                {
+                    Blocking = true
+                },
+                Terrain = ScriptableObject.CreateInstance<TerrainDefinition>()
+            };
+            Assert.True(cell.Blocked);
+        }
+    }
+}
