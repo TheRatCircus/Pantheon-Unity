5844318f7e410f48b4fb7d591e31b5b1d9c1ab57
Jerome Martina <martinaj@shernet.sheridancollege.ca>
2019-12-30 06:09:03 -0500

    Scrolling for game log

--- Assets/Scripts/UI/GameLog.cs
+++ Assets/Scripts/UI/GameLog.cs
@@ -3,13 +3,16 @@
 
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 namespace Pantheon.UI
 {
     public sealed class GameLog : MonoBehaviour, IGameLog
     {
-        [SerializeField] private Transform logTransform = default;
         [SerializeField] private GameObject msgPrefab = default;
+
+        [SerializeField] private Transform logTransform = default;
+        [SerializeField] private ScrollRect scrollRect = default;
 
         public void Send(string msg, Color color)
         {
@@ -17,6 +20,13 @@ namespace Pantheon.UI
             TextMeshProUGUI msgText = msgObj.GetComponent<TextMeshProUGUI>();
             msgText.text = msg;
             msgText.color = color;
+            StartCoroutine(ScrollToBottom());
+        }
+
+        private System.Collections.IEnumerator ScrollToBottom()
+        {
+            yield return null;
+            scrollRect.verticalNormalizedPosition = 0f;
         }
     }
 }
