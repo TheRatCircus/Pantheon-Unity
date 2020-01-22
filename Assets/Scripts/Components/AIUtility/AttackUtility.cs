9f6e64c83e292a519b5c4b3a0ab3dd1e32ff716a
Jerome Martina <martinaj@shernet.sheridancollege.ca>
2020-01-21 21:09:19 -0500

    AttackUtility involves a range check

--- Assets/Scripts/Components/AIUtility/AttackUtility.cs
+++ Assets/Scripts/Components/AIUtility/AttackUtility.cs
@@ -10,6 +10,7 @@ using System;
 namespace Pantheon.Components.Entity
 {
     using Entity = Pantheon.Entity;
+    using Talent = Pantheon.Talent;
 
     [Serializable]
     public sealed class AttackUtility : AIUtility
@@ -21,7 +22,11 @@ namespace Pantheon.Components.Entity
         public override int Recalculate(Entity entity, AI ai)
         {
             if (ai.Target == null)
-                return 0;
+                return -1;
+            int dist = entity.Level.Distance(entity.Cell, ai.Target.Cell);
+            Talent[] talents = Talent.GetAllTalents(entity);
+            if (talents[0].Range > dist)
+                return -1;
 
             return Aggression;
         }
